dc4c9c0658538473df7ddf2587672e599f162b7e
Yurii Dereguzov <DereguzovYE@yandex.ru>
2023-06-07 14:00:18 +0500

    почистить код

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-// const { celebrate, Joi } = require('celebrate');
 const userRouter = require('./users');
 const movieRouter = require('./movies');
 const { login, createUser } = require('../controllers/users');
@@ -7,24 +6,12 @@ const auth = require('../middlewares/auth');
 const NotFoundError = require('../middlewares/errors/not-found-err');
 const { validationSignup, validationSignin } = require('../middlewares/validation');
 
-// router.post('/signup', celebrate({
-//   body: Joi.object().keys({
-//     name: Joi.string().min(2).max(30).required(),
-//     email: Joi.string().required().email(),
-//     password: Joi.string().required(),
-//   }),
-// }), createUser);
 router.post('/signup', validationSignup, createUser);
 
-// router.post('/signin', celebrate({
-//   body: Joi.object().keys({
-//     email: Joi.string().required().email(),
-//     password: Joi.string().required(),
-//   }),
-// }), login);
 router.post('/signin', validationSignin, login);
 
 router.use('/users', auth, userRouter);
+
 router.use('/movies', auth, movieRouter);
 
 router.use((req, res, next) => {
--- routes/movies.js
+++ routes/movies.js
@@ -1,32 +1,11 @@
 const router = require('express').Router();
-// const { celebrate, Joi } = require('celebrate');
 const { validationCreateMovie, validationDeleteMovie } = require('../middlewares/validation');
 const { getMovies, createMovie, deleteMovie } = require('../controllers/movies');
 
 router.get('/', getMovies);
 
-// router.post('/', celebrate({
-//   body: Joi.object().keys({
-//     movieId: Joi.number().integer().required(),
-//     country: Joi.string().required(),
-//     director: Joi.string().required(),
-//     duration: Joi.number().required(),
-//     year: Joi.string().required(),
-//     description: Joi.string().required(),
-//     image: Joi.string().uri().required().pattern(/(https?:\/\/)(w{3}\.)?(([a-zA-Z0-9]+).)+/),
-//     trailerLink: Joi.string().required().pattern(/(https?:\/\/)(w{3}\.)?(([a-zA-Z0-9]+).)+/),
-//     thumbnail: Joi.string().uri().required().pattern(/(https?:\/\/)(w{3}\.)?(([a-zA-Z0-9]+).)+/),
-//     nameRU: Joi.string().required(),
-//     nameEN: Joi.string().required(),
-//   }),
-// }), createMovie);
 router.post('/', validationCreateMovie, createMovie);
 
-// router.delete('/:movieId', celebrate({
-//   params: Joi.object().keys({
-//     movieId: Joi.string().length(24).hex().required(),
-//   }),
-// }), deleteMovie);
 router.delete('/:movieId', validationDeleteMovie, deleteMovie);
 
 module.exports = router;
